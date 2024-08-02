65cc924b8b83cebec9026b7b7feb14d7dff03975
Mawunyo on wsl2 <dmashigbi@gmail.com>
2024-08-02 03:52:35 +0100

    Amend 0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
+    """
+    Determines if all boxes can be opened.
+
+    Parameters:
+    boxes (list of lists): A list where each element is a list containing keys
+                           (represented as integers) that can open other boxes.
+
+    Returns:
+    bool: True if all boxes can be opened, False otherwise.
+    """
 
 def canUnlockAll(boxes):
     n = len(boxes)
@@ -12,9 +22,9 @@ def canUnlockAll(boxes):
         # Get all the keys in the current box
         for key in boxes[current_box]:
             if key < n and key not in opened_boxes:
-                # If the key corresponds to a box number and hasn't been opened, open it
+                #key corresponds to box number and hasn't been opened, open it
                 opened_boxes.add(key)
                 queue.append(key)
-    
+
     # Check if we have opened all the boxes
     return len(opened_boxes) == n
